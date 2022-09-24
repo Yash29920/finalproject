19f4d09639201e288dd762821af9213f0e9c57db
Mohini-Nikhare <mohininikhare1112@gmail.com>
2022-09-24 10:49:19 +0530

    sop added by mohini

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -10,6 +10,7 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
                 System.out.println("Hello CJC");
                 System.out.println("Hello CJC Akurdi Pune");
+                System.out.println("added by Mohini");
 
 	}
 
